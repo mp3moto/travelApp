17dc92cf3a616a5154ef16bdd69896289b8ef9a2
Renat Gareev <mp3moto@mail.ru>
2024-03-02 22:24:25 +0500

    Initial Commit

---
+++ travelApp/travelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelAppApp.swift
+//  travelApp
+//
+//  Created by Ренат on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct travelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
